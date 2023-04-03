5e4e40f6bff966ef8364c492e528bf99efd3e412
Kitalfa <dorian.travers@yahoo.fr>
2023-04-03 14:54:43 +0200

    first commit

---
+++ Cryptocurrency App/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { Route, Link, Routes } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import './app.scss';
+
+import {
+  Exchanges,
+  Homepage,
+  News,
+  Cryptocurrencies,
+  CryptoDetails,
+  Navbar,
+} from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Cryptocurrency App/src/components/Cryptocurrencies.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Cryptocurrencies = () => {
+  return <div>ryptocurrencies</div>;
+};
+
+export default Cryptocurrencies;
